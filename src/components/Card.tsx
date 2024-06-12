313c38a9290e84b22aefadd276056835f164cefe
Simon <simonclasen1@outlook.com>
2024-06-12 10:58:13 -0300

    Adjusted image in card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, ImageSourcePropType, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { InstitutionType } from '../data/Institutions';
 
 interface CardProps {
   item: InstitutionType;
-  onPress: (item: InstitutionType)=> void;
+  onPress: (item: InstitutionType) => void;
 }
 
-function Card({ item, onPress } : CardProps)  {
+function Card({ item, onPress }: CardProps) {
   return (
-    <TouchableOpacity activeOpacity={0.8} style={styles.card} onPress={()=>onPress(item)}>
+    <TouchableOpacity activeOpacity={0.8} style={styles.card} onPress={() => onPress(item)}>
       <Image 
         source={item.image[0]}
         style={styles.image} 
@@ -26,25 +26,24 @@ function Card({ item, onPress } : CardProps)  {
 const styles = StyleSheet.create({
   card: {
     flexDirection: 'row',
-    padding: 16,
-    borderRadius: 8,
+    borderRadius: 16,
     backgroundColor: '#fff',
     shadowColor: '#000',
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
-    elevation: 4,
+    elevation: 6,
     alignItems: 'center',
     marginBottom: 16,
+    overflow: 'hidden',
   },
   image: {
-    width: 80,
-    height: 80,
-    borderRadius: 8,
-    marginRight: 16,
+    width: '33%',
+    height: '100%',
   },
   textContainer: {
     flex: 1,
+    margin: 16,
   },
   title: {
     fontSize: 18,
